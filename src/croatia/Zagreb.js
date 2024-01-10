5ea3ab84fd4621de227705bdea1cc303057f5163
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2024-01-10 03:02:07 +0000

    styled the city

--- src/croatia/Zagreb.js
+++ src/croatia/Zagreb.js
@@ -16,14 +16,13 @@ const Zagreb = () => {
           <h2>Flight: 2 hours 20 minutes</h2>
           <h2>Time difference: +1 hour</h2>
           <h2>Flight: Ryan air</h2>
-          <h2>Hotel: Double Tree by Hilton</h2>
           <h2>Pros: Cheap, Culture, Food </h2>
           <h2> cons: No Beach </h2>
         </div>
 
         <div className="zagreb-column">
           <button className="zagreb-button">
-            <a href="https://www.loveholidays.com/holidays/?destinationIds=1020&departureAirports=LON&nights=3&rooms=2&date=2024-07-05&flexibility=0&sort=POPULAR&f.boardBasis=BB&f.pools=Indoor%20Pool" target="_blank" rel="noreferrer"> View Search results </a>
+            <a href="https://www.loveholidays.com/holidays/?destinationIds=2249&departureAirports=LON&nights=3&rooms=2&date=2024-07-05&flexibility=0&sort=POPULAR&f.boardBasis=BB&f.pools=Indoor%20Pool" target="_blank" rel="noreferrer"> View Search results </a>
           </button>
 
           <div>
--- src/spain/Marbella.js
+++ src/spain/Marbella.js
@@ -1,8 +1,8 @@
 
 import React from "react";
 import "./Marbella.css";
-import marbellaImage from "../images/marbella.png";
-import marbellaImage1 from "../images/marbella2.png";
+import marbellaImage from "../images/marbella-collage.png";
+
 import BackButton from "../back-button/Back-button";
 
 const Marbella= () => {
@@ -10,22 +10,20 @@ const Marbella= () => {
 <div className="marbella">
 <BackButton />
 
-<h1>Marbella 🇪🇸</h1>
+<img className="marbella-screenshot" src={marbellaImage} id="marbella-images" alt='marbella'/>
+
+<div className="marbella-content">
+<div className="marbella-column" id="marbella">
 <h2>Weather: 28°</h2>
 <h2>Flight: 2 hours 20 minutes</h2>
 <h2>Time difference: +1 hour</h2>
 <h2>Flight: EasyJet</h2>
+</div>
+<div className="marbella-column" >
+<button className="marbella-button">
+            <a href="https://www.loveholidays.com/holidays/?destinationIds=1020&departureAirports=LON&nights=3&rooms=2&date=2024-07-05&flexibility=0&sort=POPULAR&f.boardBasis=BB&f.pools=Indoor%20Pool" target="_blank" rel="noreferrer"> View Search results </a>
+          </button>
 
-
-
-<hr/>
-<br/>
-<h2>From easyjet.com (£50 discount pp will apply)</h2>
-<img className="marbella-screenshot" src={marbellaImage} alt='marbella'/>
-<br/>
-<hr/>
-<h2>from loveholidays.com </h2>
-<img className="marbella-screenshot" src={marbellaImage1} alt='marbella'/>
 <div>
 <button className= "marbella-button">
 <a href="https://www.getyourguide.co.uk/-l1217/ttd/?cmp=ga&ps_theme=ttd&cq_src=google_ads&cq_cmp=15505300953&cq_con=133873374387&cq_term=things+to+do+in+marbella&cq_med=&cq_plac=&cq_net=g&cq_pos=&cq_plt=gp&campaign_id=15505300953&adgroup_id=133873374387&target_id=kwd-4028074578&loc_physical_ms=9046212&match_type=e&ad_id=581827598839&keyword=things+to+do+in+marbella&ad_position=&feed_item_id=&placement=&device=c&partner_id=CD951&gad_source=1&gclid=CjwKCAiA-vOsBhAAEiwAIWR0TZUKbeTn4Q5R47jkYL5eaU6COqS6QLQi_YGTbSWIgo26A0sEB3ZoMhoCmTMQAvD_BwE&visitor-id=VIJ30F8YQ7OF7VK3KICF6BPZC7WGJIDL&locale_autoredirect_optout=true" target ="_blank" rel="noreferrer">Things to do in Marbella</a>
@@ -35,7 +33,8 @@ const Marbella= () => {
 <a href="https://www.tripadvisor.co.uk/Hotel_Review-g187439-d568934-Reviews-Senator_Marbella_Spa_Hotel-Marbella_Costa_del_Sol_Province_of_Malaga_Andalucia.html" target ="_blank" rel="noreferrer">Hotel Review</a>
 </button>
 </div>
-
+</div>
+</div>
 </div>
     )
 }
